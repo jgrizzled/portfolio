97da336c34105e924dbb7a1fee0f0bce70d3bdae
jgrizzled <justin.greene5@gmail.com>
2019-11-11 21:25:36 -0700

    hamburger menu, style fixes, favicon

--- js/app.js
+++ js/app.js
@@ -1,18 +1,60 @@
 'use strict';
 $(() => {
   const win = $(window);
-  const nav = $('nav');
+  const documentHeight = $(document).height();
+  const root = $(':root');
+  const nav = $('#nav-bar').add('.burger-item');
+  const burgerMenu = $('#burger-menu');
+  const burgerToggle = $('#burger-toggle');
+  const burgerLinks = $('.burger-item a');
+  const notBurgerLinks = $('*').not(burgerLinks);
+  let burgerOpened = false;
 
-  //set <nav> background opacity between 10% and 100% based on scroll position
-  function setNavScrollOpacity() {
-    nav.css('background', `rgba(17, 17, 17, ${
-      Math.min(parseFloat(win.scrollTop()/100+0.1).toFixed(2),1)
-    })`);
+  //set nav background opacity between 15% and 100% based on scroll position
+  function navScrollOpacity() {
+    setNavOpacity(win.scrollTop() / (documentHeight*0.05) + 0.15);
   }
 
-  setNavScrollOpacity();
-
-  win.on('scroll', () => {
-      setNavScrollOpacity();
+  function setNavOpacity(opacity) {
+    nav.css('background',
+      'rgba(17, 17, 17, ' +
+        Math.min(
+          parseFloat(opacity)
+            .toFixed(2),
+          1) +
+      ')'
+    );
+  }
+ 
+  function toggleBurgerMenu(event) {
+    event.stopPropagation();
+    if(!burgerOpened) {
+      nav.css('transition', 'background .15s ease-in-out');
+      nav.css('background', root.css('--darker-charcoal'));
+      burgerMenu.css('right', '0');
+      burgerMenu.attr('aria-hidden', 'false');
+      burgerLinks.attr('tabindex', '0');
+      notBurgerLinks.one('click', toggleBurgerMenu);
+      burgerOpened = true;
+    } else {
+      burgerMenu.css('right', '-100%');
+      navScrollOpacity();
+      burgerMenu.attr('aria-hidden', 'true');
+      burgerLinks.attr('tabindex', '-1');
+      notBurgerLinks.off('click');
+      burgerToggle.one('click', toggleBurgerMenu);
+      burgerOpened = false;
+      nav.css('transition', 'none');
+    }
+  }
+  
+  win.on('scroll', (event) => {
+    if(!burgerOpened)
+      navScrollOpacity();
+    else //close burger menu on scroll
+      toggleBurgerMenu(event);
   });
+
+  navScrollOpacity(); //initialize nav opacity
+  burgerToggle.one('click', toggleBurgerMenu);
 });
